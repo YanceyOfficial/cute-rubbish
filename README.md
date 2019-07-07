ea0c33885a39fe7dc29d34f3e1467024b7e068a0
yanceyleo <developer@yanceyleo.com>
2019-07-07 13:36:54 +0800

    feat: Init repo

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# cute-rubbish
-【拯救上海市民向】圾你太美
+# 圾你太美
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-orange.svg)](https://opensource.org/licenses/MIT)
+[![Node](https://img.shields.io/badge/node-%3E%3D8.0.0-blue.svg)](https://github.com/YanceyOfficial/cute-rubbish)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-green.svg)](https://github.com/YanceyOfficial/cute-rubbish/pulls)
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
+
+Runs the app in the Terminal and input the name of rubbish.<br>
+You will also see related info in the console.
+
+## License
+
+Cute Rubbish is [MIT licensed](https://opensource.org/licenses/MIT).
---
+++ index.js
@@ -0,0 +1,81 @@
+const Crawler = require('crawler');
+const chalk = require('chalk');
+const readline = require('readline');
+const log = console.log;
+const baseUrl =
+  'http://trash.lhsr.cn/sites/feiguan/trashTypes_2/TrashQuery.aspx?kw=';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const sortRubbish = kw => {
+  const crawler = new Crawler({
+    maxConnections: 10,
+    callback(error, res, done) {
+      if (error) {
+        log(chalk.bold.red(error));
+      } else {
+        const $ = res.$;
+
+        const target = chalk.blue.bold(
+          $('.info')
+            .text()
+            .trim(),
+        );
+
+        if (target) {
+          const title = `${chalk.white(kw)}是${target}`;
+
+          const desc = `${target}是指: ${chalk.yellow(
+            $('.title div')
+              .text()
+              .trim()
+              .split('：')[1],
+          )}`;
+
+          const include = `${target}主要包括: ${chalk.red(
+            $('.desc')
+              .text()
+              .trim(),
+          )}`;
+
+          const tips = Object.values($('li'))
+            .map(val => val.children)
+            .map(val => (Array.isArray(val) ? val[0].data : null));
+
+          const requrst = `${target}投放要求: ${chalk.magenta(
+            tips.filter(f => f).join('; '),
+          )}`;
+
+          log(
+            '\n\n' +
+              title +
+              '\n\n' +
+              desc +
+              '\n\n' +
+              include +
+              '\n\n' +
+              requrst +
+              '\n',
+          );
+        } else {
+          log('暂未收录该垃圾');
+        }
+
+        done();
+        rl.close();
+      }
+    },
+  });
+  crawler.queue(encodeURI(baseUrl + kw));
+};
+
+rl.question('Iuput the name of rubbish. >>> ', function(answer) {
+  sortRubbish(answer);
+});
+
+rl.on('close', function() {
+  process.exit(0);
+});
